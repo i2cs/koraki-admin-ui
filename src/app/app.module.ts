6a914896a67c7fbfb9bc4f50c2e710db37b2e0f0
madushak <poosathecat@gmail.com>
2018-06-16 11:23:54 +0530

    Added authentication service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
-import { HttpModule } from '@angular/http';
 import { APP_BASE_HREF } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -43,13 +42,16 @@ import { AppComponent } from './app.component';
 
 import { SidebarModule } from './sidebar/sidebar.module';
 import { FooterModule } from './shared/footer/footer.module';
-import { NavbarModule} from './shared/navbar/navbar.module';
+import { NavbarModule } from './shared/navbar/navbar.module';
 import { AdminLayoutComponent } from './layouts/admin/admin-layout.component';
 import { AuthLayoutComponent } from './layouts/auth/auth-layout.component';
 
 import { AppRoutes } from './app.routing';
 import { HttpClientModule } from '@angular/common/http';
-import { Configuration, ConfigurationParameters, ApiModule } from 'koraki-angular-client';
+import { ApiModule, Configuration, ConfigurationParameters } from 'koraki-angular-client';
+import { environment } from 'environments/environment';
+import { AuthService } from './services/auth.service';
+import { AngularWebStorageModule } from 'angular-web-storage';
 
 @NgModule({
   exports: [
@@ -87,39 +89,34 @@ import { Configuration, ConfigurationParameters, ApiModule } from 'koraki-angula
   ],
   declarations: []
 })
-export class MaterialModule {}
-
-export function apiConfigFactory (): Configuration {
-  const params: ConfigurationParameters = {
-    // set configuration parameters here.
-    apiKeys : {"Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IlFUVTVNVVV6TURKQ05UZzRSRGMzT1VKRlFVRkdPRFpFUVRnM09VTTFNVEF5TmtGQk1EZzVRZyJ9.eyJlbWFpbCI6InNhc3NyaS51Y3NjQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiaXNzIjoiaHR0cHM6Ly9rb3Jha2kuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDViMTgxODQ0YTc2YjcwMjE2OTI0OWVmNSIsImF1ZCI6ImduMHNGM3ZnOHppQVdOUzNFemRISVlSMjZ4NTU2NFZuIiwiaWF0IjoxNTI4NjM2Mjc3LCJleHAiOjE1Mjk1MDAyNzcsImF0X2hhc2giOiJmeWJ4N1VWYURrM0ZnQUdKaVlwMHlRIiwibm9uY2UiOiJaTng0Ry16QUdrREZFYzFDdGFjd3J3ODlDS2N5OE5tWiJ9.Ow92bpHJkmxkyBbC-It9gsXvSVKvNbUMP_FQR56Gnhe3241z5rlXmtyGiUCEBAALE1UyeIshir1posRG_KFFtjEmDKWhKrzmxlm33JqOvqW6eh-2JoC_DEB6-kGvoxbYg5rWKGXEVyZbeiOACFvyVXKh_Y3seqnvLH9ZTqKMjbtz_wT5e8TLBXOjFMv1ZD5vHK5xFGO5zUyaKdpUzzvXl6cYLRhGNH6RM4x2n1CfQRJqW3T3FbKM3WYHMTmvfUswHaY2oelFo1Vn65Dq80i3Y0-uhhVm6jnfuQ36V29t0UTttMUiXu3VPOxiv2Vzfx8r_iiR1ZF2rytWfJQZ-mPNeg"},
-    withCredentials : true,
-    basePath : "http://localhost:5000"
-  }
-  return new Configuration(params);
-}
+export class MaterialModule { }
 
 @NgModule({
-    imports:      [
-        CommonModule,
-        BrowserAnimationsModule,
-        FormsModule,
-        RouterModule.forRoot(AppRoutes),
-        HttpModule,
-        MaterialModule,
-        MatNativeDateModule,
-        SidebarModule,
-        NavbarModule,
-        FooterModule,
-        HttpClientModule,
-        ApiModule.forRoot(apiConfigFactory)
-    ],
-    declarations: [
-        AppComponent,
-        AdminLayoutComponent,
-        AuthLayoutComponent
-    ],
-    //providers: [{ provide: BASE_PATH, useValue: "http://localhost:5000" }],
-    bootstrap:    [ AppComponent ]
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    RouterModule.forRoot(AppRoutes),
+    MaterialModule,
+    MatNativeDateModule,
+    SidebarModule,
+    NavbarModule,
+    FooterModule,
+    HttpClientModule,
+    AngularWebStorageModule,
+    ApiModule
+  ],
+  declarations: [
+    AppComponent,
+    AdminLayoutComponent,
+    AuthLayoutComponent
+  ],
+  providers: [{
+    provide: Configuration,
+    useFactory: (authSvc: AuthService) => authSvc.getAuthConfig(),
+    deps: [AuthService],
+    multi: false
+  }],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
