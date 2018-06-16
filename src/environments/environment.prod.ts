e8ae2cd0863380f7ea085e5c72f85cc9c73b881a
madushak <poosathecat@gmail.com>
2018-06-16 17:15:23 +0530

    Corrected prod environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,13 +2,13 @@ export const environment = {
   production: true,
   apiBaseUrl: "https://koraki-api-backend.azurewebsites.net/",
   auth: {
-    clientID: '[YOUR_AUTH0_CLIENT_ID]',
-    domain: '[YOUR_AUTH0_DOMAIN]', // e.g., [you].auth0.com
-    redirect: 'http://localhost:4200/callback',
-    audience: '[YOUR_AUTH0_API]', // e.g., http://localhost:3001/api/
-    scope: '[YOUR_AUTH0_SCOPES]', // e.g., openid profile email
-    namespace: '[YOUR_AUTH0_NAMESPACE]', // e.g., http://myapp.com/roles,
-    autoRenewToken: true, // boolean
-    dynamicRedirect: true // boolean
+    clientID: 'gn0sF3vg8ziAWNS3EzdHIYR26x5564Vn',
+    domain: 'koraki.auth0.com',
+    redirect: 'https://koraki-admin-ui.herokuapp.com/auth/callback',
+    audience: 'https://koraki.auth0.com/userinfo',
+    scope: 'openid email',
+    namespace: 'http://koraki.io',
+    autoRenewToken: true,
+    dynamicRedirect: true
   }
 };
