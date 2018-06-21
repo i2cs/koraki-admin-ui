0a0037a38ceff55b346d7433c9c99fbc2785b055
madushak <poosathecat@gmail.com>
2018-06-21 23:49:39 +0530

    Added https endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   auth: {
     clientID: 'gn0sF3vg8ziAWNS3EzdHIYR26x5564Vn',
     domain: 'koraki.auth0.com',
-    redirect: 'http://app.koraki.io/auth/callback',
+    redirect: 'https://app.koraki.io/auth/callback',
     audience: 'https://koraki.auth0.com/userinfo',
     scope: 'openid email',
     namespace: 'http://koraki.io',
