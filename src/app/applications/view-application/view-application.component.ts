256c61bccadc2d7ba6febf138ab3d8da1bd2079b
madushak <poosathecat@gmail.com>
2018-06-25 15:48:22 +0530

    UI changes

--- src/app/applications/view-application/view-application.component.ts
+++ src/app/applications/view-application/view-application.component.ts
@@ -6,16 +6,33 @@ import { NotificationService } from '../../services/notification.service';
 
 declare const $: any;
 
+interface _Iterable extends Iterable<{}> {
+  length: number;
+}
+
+class _Array<T> extends Array<T> {
+  static range(from: number, to: number, step: number): number[] {
+      return Array.from(
+          (<_Iterable>{ length: Math.floor((to - from) / step) + 1 }),
+          (v, k) => from + k * step
+      );
+  }
+}
+
 @Component({
   selector: 'app-view-application',
   templateUrl: './view-application.component.html',
   styleUrls: ['./view-application.component.scss']
 })
+
 export class ViewApplicationComponent implements OnInit {
+  
   hide: boolean;
   application: ApplicationViewDataModel = <ApplicationViewDataModel>{};
   script: string;
   status: boolean;
+  updatingSettings: boolean;
+  possibleCounts: number[] = _Array.range(8, 100, 1);
 
   constructor(
     private route: ActivatedRoute,
@@ -34,7 +51,7 @@ export class ViewApplicationComponent implements OnInit {
           this.loadingService.loading(false);
           this.application = a;
           this.status = a.status == "Active";
-          this.script = "<script>window.sparkleSettings = { app_id: \"" + a.clientId + "\", analytics_id: \"" + a.analyticsId +"\" }; !function(){function t(){var t=a.createElement(\"script\"); t.type=\"text/javascript\", t.async=!0,t.src=\"\/\/api.koraki.io/widget.js\"; var e=a.getElementsByTagName(\"script\")[0];e.parentNode.insertBefore(t,e)} var e=window,a=document;e.attachEvent?e.attachEvent(\"onload\",t):e.addEventListener(\"load\",t,!1)}();</script>"
+          this.script = "<script>window.sparkleSettings = { app_id: \"" + a.clientId + "\", analytics_id: \"" + a.analyticsId + "\" }; !function(){function t(){var t=a.createElement(\"script\"); t.type=\"text/javascript\", t.async=!0,t.src=\"\/\/api.koraki.io//widget/v1.0/js\"; var e=a.getElementsByTagName(\"script\")[0];e.parentNode.insertBefore(t,e)} var e=window,a=document;e.attachEvent?e.attachEvent(\"onload\",t):e.addEventListener(\"load\",t,!1)}();</script>"
         }, e => {
           this.loadingService.loading(false);
         });
@@ -48,21 +65,38 @@ export class ViewApplicationComponent implements OnInit {
     });
   }
 
-  updateApplicationStatus(){
+  updateApplicationStatus() {
     this.loadingService.loading(true);
-      let status : ApplicationUpdateDataModel.StatusEnum = this.status ? ApplicationUpdateDataModel.StatusEnum.Active : ApplicationUpdateDataModel.StatusEnum.Disabled;
-      this.appservice.updateApplication(this.application.id, <ApplicationUpdateDataModel>{ status : status }).subscribe(a => {
-        this.loadingService.loading(false);
-        this.application = a;
-        this.notify.success("Application is " + status.toString());
-      }, e => {
-        this.loadingService.loading(false);
-      });
+    let status: ApplicationUpdateDataModel.StatusEnum = this.status ? ApplicationUpdateDataModel.StatusEnum.Active : ApplicationUpdateDataModel.StatusEnum.Disabled;
+    this.appservice.updateApplication(this.application.id, <ApplicationUpdateDataModel>{ status: status }).subscribe(a => {
+      this.loadingService.loading(false);
+      this.application = a;
+      this.notify.success("Application is " + status.toString());
+    }, e => {
+      this.loadingService.loading(false);
+    });
   }
 
-  deleteApplication(){
+  updateApplicationSettings() {
+    this.updatingSettings = true;
+    this.loadingService.loading(true);
+    this.appservice.updateApplication(this.application.id, <ApplicationUpdateDataModel>{
+      notificationMaximumActiveCount: this.application.notificationMaximumActiveCount,
+      notificationMaximumDurationDays: this.application.notificationMaximumDurationDays
+    }).subscribe(a => {
+      this.updatingSettings = false;
+      this.loadingService.loading(false);
+      this.application = a;
+      this.notify.success("Application settings saved");
+    }, e => {
+      this.updatingSettings = false;
+      this.loadingService.loading(false);
+    });
+  }
+
+  deleteApplication() {
     var result = confirm("Are you sure you want to delete this application? This action is not reversible");
-    if(result){
+    if (result) {
       this.loadingService.loading(true);
       this.appservice.deleteApplication(this.application.id).subscribe(a => {
         this.loadingService.loading(false);
